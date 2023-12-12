0d99f08bc61933afaf0e5a4f79a682bfe5ed1610
Wallace Carvalho <contato@wallace.cafe>
2023-12-12 18:23:43 -0300

    site em desenvolvimento

---
+++ app/Views/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wallace - Programador FullStack</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body style="background-color:#f0f3d6ff">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center pt-5 mt-5">
+                <p class="mb-1">Estou construindo uma paradinha bem legal...</p>
+                <p class="mb-5">Atualizo aqui em breve! 😎</p>
+                <img class="img-fluid" src="<?= base_url('assets/img/buildando.jpg'); ?>" alt="">
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
